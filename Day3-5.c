57d13fd1df0c488a44011264e04cd9b69fbdff59
Manas <manas@manass-MacBook-Air-2.local>
2026-08-25 23:54:06 +0530

    Add Day3-5 C program

---
+++ Day3-5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int celsius, fahrenheit;
+    printf("Enter temperature in Celsius: ");
+    scanf("%d", &celsius);
+    fahrenheit = (celsius * 9 / 5) + 32;
+    printf("Temperature in Fahrenheit: %d\n", fahrenheit);
+    return 0;
+}
