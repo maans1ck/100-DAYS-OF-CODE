94a410d9f1e373e91fc4cb976f573ab61b78e8e5
maans1ck <manaswork1358@gmail.com>
2026-09-05 15:47:52 +0530

    Add Day12-23 C program

---
+++ Day12-23.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int daysLate;
+    int fine = 0;
+
+    printf("Enter the number of days late: ");
+    scanf("%d", &daysLate);
+
+    if (daysLate <= 0)
+    {
+        printf("No fine");
+    }
+    else if (daysLate <= 5)
+    {
+        fine = daysLate * 2;
+        printf("Fine = Rs. %d", fine);
+    }
+    else if (daysLate <= 10)
+    {
+        fine = (5 * 2) + ((daysLate - 5) * 4);
+        printf("Fine = Rs. %d", fine);
+    }
+ 
+    else if (daysLate <= 30)
+    {
+        fine + (5 * 2) + (5 * 4) + ((daysLate - 10) * 6);
+        printf("Fine = Rs. %d", fine);
+    }
+    else 
+    {
+        printf("Membership cancelled");
+    }
+
+    return 0;
+}
